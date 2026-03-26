5560e1979f829310d105a49b0859fdffe1b9863f
Belidet <166855181+Belidet@users.noreply.github.com>
2026-03-27 02:46:26 +0300

    Update inspiration.js

--- api/inspiration.js
+++ api/inspiration.js
@@ -1,23 +1,11 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const path = require('path');
+const { db } = require('./_lib/db');
 
-const dbPath = path.join(process.cwd(), 'db.json');
-const adapter = new FileSync(dbPath);
-const db = low(adapter);
-
-module.exports = (req, res) => {
-  // Enable CORS
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
   
   const inspirations = db.get('inspirations').value();
   const random = inspirations[Math.floor(Math.random() * inspirations.length)];
